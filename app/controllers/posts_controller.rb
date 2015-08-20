2c3142276228a721b8b0a274e0932181e4422dad
Junko Werner <aliciajace@gmail.com>
2015-08-19 17:01:10 -0700

    style, new form,Edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,47 @@
 class PostsController < ApplicationController
 	def index
-		@post = Post.all.order('created_at DESC')
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
+		@post = Post.new
+
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render :new
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post, notice: "Yeah~ You udated '#{@post.title}' successfully."
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		if @post.destroy
+			redirect_to root_path, notice: 'You has deleted the post.'
+		else
+			render :show
+		end
+	end
 
 	private
 	 def post_params
